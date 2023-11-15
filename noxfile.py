6714913e38d54c5c32a4322d664703a903197989
mw6136 <mw6136@princeton.edu>
2023-11-15 11:37:48 -0500

    add documentation, Python version, Ruff to CI and tests

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python="3.10")
+@nox.session(python="3.11")
 def tests(session: nox.Session) -> None:
     """
     Run the unit and regular tests.
@@ -10,18 +10,17 @@ def tests(session: nox.Session) -> None:
     session.run("pytest")
 
 
-@nox.session(python="3.10")
+@nox.session(python="3.11")
 def docs(session: nox.Session) -> None:
     """
     Build the docs. Pass "--serve" to serve.
     """
-
     session.install(".[docs]")
     session.chdir("docs")
     session.run("sphinx-build", "-M", "html", ".", "_build")
 
 
-@nox.session(python="3.10")
+@nox.session(python="3.11")
 def serve(session: nox.Session) -> None:
     docs(session)
     print("Launching docs at http://localhost:8000/ - use Ctrl-C to quit")
