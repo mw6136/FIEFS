23285ccd96d65f05d7aafa5c3748af348602eb3c
Philip Satterthwaite <ps1639@princeton.edu>
2023-12-11 20:56:00 -0500

    Added GUI tabs for geometry and time parameters, restructured GUI logic for more efficient copying of code, implemented writing-to-file capability

--- GUI/flow_parameters_tab.py
+++ GUI/flow_parameters_tab.py
@@ -1,47 +1,66 @@
 from tkinter import ttk
+import os
 
+def is_valid_float(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
+def value_changed(event, line_number, char_number, entry, error_label):
+    new_value = entry.get()
+
+    # Check if the entered value is a valid float
+    if not is_valid_float(new_value):
+        error_label.config(text=f"\"{new_value}\" is not a valid float.", foreground="red", font=("Arial",12))
+        return
+    
+
+    error_label.config(text="✓",foreground="green", font=("Arial", 16))
+
+    file_path = os.path.abspath(os.path.join("inputs", "GUI_input_file.txt"))
+    
+    # Read existing lines from the file
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    # Update the substring starting from the specified index in each line
+    if line_number < len(lines):
+        line = lines[line_number]
+        if char_number < len(line):
+            # Delete everything after the specified character index
+            lines[line_number] = line[:char_number] + new_value + '\n'
+
+            # Write the modified lines back to the file
+            with open(file_path, 'w') as file:
+                file.writelines(lines)
+
+            print(f"New value entered and saved: {new_value}")
+        else:
+            print(f"Invalid character index for line {line_number}: {char_number}")
+    else:
+        print(f"Invalid line number: {line_number}")
 def create_flow_parameters_tab(notebook):
     frame = ttk.Frame(notebook, width=300, height=200)
     frame.grid(row=0, column=0, sticky="nsew")
     notebook.add(frame, text="Flow Parameters")
 
-    # Labels
-    rho0_label = ttk.Label(frame, text="rho0:")
-    rho0_label.grid(row=0, column=0, padx=10, pady=10, sticky="w")
+    # Variables and line numbers in the file
+    variables_and_lines = {"rho0": [14, 8], "rho1": [15, 8], "p0": [17, 8], "p1": [18, 8],
+                           "u0": [20, 8], "u1": [21, 8], "pert_amp": [22, 8]}
 
-    rho1_label = ttk.Label(frame, text="rho1:")
-    rho1_label.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+    # Labels, Entry widgets, and Error labels
+    for variable, index in variables_and_lines.items():
+        label = ttk.Label(frame, text=f"{variable}:")
+        label.grid(row=index[0], column=0, padx=10, pady=10, sticky="w")
 
-    p0_label = ttk.Label(frame, text="p0:")
-    p0_label.grid(row=2, column=0, padx=10, pady=10, sticky="w")
+        entry = ttk.Entry(frame)
+        entry.grid(row=index[0], column=1, padx=10, pady=10)
 
-    p1_label = ttk.Label(frame, text="p1:")
-    p1_label.grid(row=3, column=0, padx=10, pady=10, sticky="w")
+        error_label = ttk.Label(frame, text="", foreground="red")
+        error_label.grid(row=index[0], column=2, padx=10, pady=10)
 
-    u0_label = ttk.Label(frame, text="u0:")
-    u0_label.grid(row=4, column=0, padx=10, pady=10, sticky="w")
+        entry.bind("<FocusOut>", lambda event, line=index[0], char=index[1], ent=entry, err_lbl=error_label: value_changed(event, line, char, ent, err_lbl))
 
-    u1_label = ttk.Label(frame, text="u1:")
-    u1_label.grid(row=5, column=0, padx=10, pady=10, sticky="w")
-
-    # Entry widgets for user input
-    rho0_entry = ttk.Entry(frame)
-    rho0_entry.grid(row=0, column=1, padx=10, pady=10)
-
-    rho1_entry = ttk.Entry(frame)
-    rho1_entry.grid(row=1, column=1, padx=10, pady=10)
-
-    p0_entry = ttk.Entry(frame)
-    p0_entry.grid(row=2, column=1, padx=10, pady=10)
-
-    p1_entry = ttk.Entry(frame)
-    p1_entry.grid(row=3, column=1, padx=10, pady=10)
-
-    u0_entry = ttk.Entry(frame)
-    u0_entry.grid(row=4, column=1, padx=10, pady=10)
-
-    u1_entry = ttk.Entry(frame)
-    u1_entry.grid(row=5, column=1, padx=10, pady=10)
-
-
-    return frame  # Don't forget to return the frame
+    return frame
