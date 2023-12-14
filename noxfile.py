d011986c923e373f2cfac432d4f840aeb08347f6
Philip Satterthwaite <ps1639@princeton.edu>
2023-12-13 19:06:42 -0500

    debugging tests 13

--- noxfile.py
+++ noxfile.py
@@ -1,9 +1,10 @@
 import nox
 import os
-import sys
 
+# Define the versions you want to test against
+python_versions = ["3.9", "3.10", "3.11"]
 
-@nox.session(python=["3.9", "3.10", "3.11"])
+@nox.session(python=python_versions)
 def tests(session: nox.Session) -> None:
     """
     Run the unit and regular tests.
@@ -11,12 +12,10 @@ def tests(session: nox.Session) -> None:
     session.install(".[test]")
     session.run("pytest")
 
-
 @nox.session(python="3.11")
 def lint(session):
     session.install("flake8")
     session.run("flake8", "src/")
-
 
 @nox.session(python="3.11")
 def docs(session: nox.Session) -> None:
@@ -26,7 +25,6 @@ def docs(session: nox.Session) -> None:
     session.install(".[docs]")
     session.chdir("docs")
     session.run("sphinx-build", "-M", "html", ".", "_build")
-
 
 @nox.session(python="3.11")
 def serve(session: nox.Session) -> None:
