c757c53efde46492a9cb415aa51130cbba4f7d7a
Philip Satterthwaite <ps1639@princeton.edu>
2023-12-13 19:14:37 -0500

    debugging tests 15

--- noxfile.py
+++ noxfile.py
@@ -4,25 +4,20 @@ import os
 # Define the versions you want to test against
 python_versions = ["3.9", "3.10", "3.11"]
 
-#@nox.session(python=python_versions)
-#def tests(session: nox.Session) -> None:
-#    """
-#    Run the unit and regular tests.
-#    """
-#    session.install(".[test]")
-#    session.run("pytest")
-
-@nox.session(python="/path/to/python3.11")
+@nox.session(python=python_versions)
 def tests(session: nox.Session) -> None:
+    """
+    Run the unit and regular tests.
+    """
     session.install(".[test]")
     session.run("pytest")
 
-@nox.session(python="3.11")
+@nox.session(python="3.10")
 def lint(session):
     session.install("flake8")
     session.run("flake8", "src/")
 
-@nox.session(python="3.11")
+@nox.session(python="3.10")
 def docs(session: nox.Session) -> None:
     """
     Build the docs. Pass "--serve" to serve.
@@ -31,7 +26,7 @@ def docs(session: nox.Session) -> None:
     session.chdir("docs")
     session.run("sphinx-build", "-M", "html", ".", "_build")
 
-@nox.session(python="3.11")
+@nox.session(python="3.10")
 def serve(session: nox.Session) -> None:
     docs(session)
     print("Launching docs at http://localhost:8000/ - use Ctrl-C to quit")
